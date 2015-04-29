2b0ebd9f05e76bf52a0c28cfe7edab5151e7f786
BumpinBursaw <allanbursaw@gmail.com>
2015-04-28 21:58:40 -0400

    Phase 3

---
+++ War.java
@@ -0,0 +1,9 @@
+public class War
+{
+   public static void main(String [] args) 
+   {
+      Deck warDeck = new Deck();
+      QueueReferenceBased Player1 = new QueueReferenceBased();
+      QueueReferenceBased Player2 = new QueueReferenceBased();
+   }
+}
