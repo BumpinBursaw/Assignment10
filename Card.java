84bbb221b7f4ab551067b53cec82865f45d2a63d
BumpinBursaw <allanbursaw@gmail.com>
2015-04-28 21:28:26 -0400

    Card and Deck update
    Removed enum and added the deck class

--- Card.java
+++ Card.java
@@ -6,20 +6,32 @@
 
 public class Card
 {
-   private CardRank Rank;     //used to hold enum card rank
-   private CardSuit Suit;     //used to hold enum card rank
-   private int rank;          //holds the rank as an integer 
-   private int suit;          //holds the suit as an integer
+   private int rank;                      //holds the rank as an integer 
+   private int suit;                      //holds the suit as an integer
+   public static final int ACE = 14;
+   public static final int QUEEN = 13;
+   public static final int KING = 12;
+   public static final int JACK = 11;
+   
+   public static final int SPADES = 1;
+   public static final int HEARTS = 2;    
+   public static final int CLUBS = 3;     
+   public static final int DIAMONDS = 4;  
+   
+   public String rankString;              //holds the rank as an string
+   public String suitString;              //holds the rank as an string
+   
+   
    
    /**
    This Card constructor accept the suit and rank as argument
    @param s suit
    @param r rank
    */
-   public Card(CardRank Rank, CardSuit Suit)
+   public Card(int rank, int suit)
    {
-      this.Rank = Rank;
-      this.Suit = Suit;
+      this.rank = rank;
+      this.Suit = suit;
          
    }
 
@@ -27,24 +39,63 @@ public class Card
    This getSuit mehtod return the Suit to the user as an int
    @return suit the suit of the card
    */
-   public int getSuit()
+   public int getRank()
    {
-      switch (Suit)
+      switch(suit)
       {
-         case SPADES:
-            suit = 1;
-            break;
-         case CLUBS:
-            suit = 2;
-            break;
-         case HEARTS:
-            suit = 3;
-            break;
-         case DIAMONDS:
-            suit = 4;
-            break;
+         case 2:
+         rankString = "Two";
+         break;
+         
+         case 3:
+         rankString = "Three";
+         break;
+         
+         case 4:
+         rankString = "Four";
+         break;
+         
+         case 5:
+         rankString = "Five"
+         break;
+         
+         case 6:
+         rankString = "Six"
+         break;
+         
+         case 7:
+         rankString = "Seven"
+         break;
+         
+         case 8:
+         rankString = "Eight"
+         break;
+         
+         case 9:
+         rankString = "Nine"
+         break;
+         
+         case 10:
+         rankString = "Ten"
+         break;
+         
+         case JACK:
+         rankString = "Jack"
+         break;
+         
+         case QUEEN:
+         rankString = "Queen"
+         break;
+         
+         case KING:
+         rankString = "King"
+         break;
+         
+         case ACE:
+         rankString = "Ace"
+         break;
       }
-      return suit;
+      return rankString;
    }
       
    /**
@@ -53,59 +104,33 @@ public class Card
    */
    public int getRank()
    {
-      switch (Rank)
+      switch(suit)
       {
-         case TWO:
-            rank = 1;
-            break;
-         case THREE:
-            rank  = 2;
-            break;
-         case FOUR:
-            rank = 3;
-            break;
-         case FIVE:
-            rank = 4;
-            break;
-         case SIX:  
-            rank = 5;
-            break;
-         case SEVEN:
-            rank = 6;
-            break;
-         case EIGHT:
-            rank = 7;
-            break;
-         case NINE:
-            rank = 8;
-            break;
-         case TEN:
-            rank = 9;
-            break;
-         case JACK:
-            rank = 10;
-            break;
-         case QUEEN:
-            rank = 11;
-            break;
-         case KING:
-            rank = 12;
-            break;
-         case ACE:
-            rank = 13;
-            break;
-         default:
-            rank = 1;
-            break;
+         case SPADES:
+         suitString = "Spades"
+         break;
+         
+         case HEARTS:
+         suitString = "Hearts"
+         break;
+         
+         case CLUBS:
+         suitString = "Clubs"
+         break;
+         
+         case DIAMONDS:
+         suitString = "Diamonds"
+         break;
       }
-      return rank;
+      return suitString;
    }
    
    /**
    This toString method return a string to print out
    */
-   public String toString(){
-      return "\nThis card is a " + Rank + " of " + Suit;
+   public String toString()
+   {
+      return "\nThis card is a " + rankString + " of " + suitString;
    }
    
    /**
